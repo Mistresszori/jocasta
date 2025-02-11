92506f5336b9526169074c784cba939f623af2f0
Mistresszori <gaja.da.frize@gmail.com>
2025-02-11 21:58:52 +0000

    Add files via upload

---
+++ scripts/musicas.js
@@ -0,0 +1,4 @@
+const musicas = [
+    { name: "De manhã Carlos, à noite Clarisse", artist: "Desinteria", url: "musicas/De-manha-Carlos-a-noite-Clarisse.mp3" },
+    { name: "De manhã Carlos, à noite Clarisse2", artist: "Artista 2", url: "musicas/De-manha-Carlos-a-noite-Clarisse.mp3" }
+];
